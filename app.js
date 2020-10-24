6e9a5260a916e70d045bc8a3a85f4be7e19a8500
Ru <rudikraeher@gmail.com>
2020-10-23 19:08:03 -0700

    set up main menu inquirer prompt and view all employees query.

---
+++ app.js
@@ -0,0 +1,91 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const dotenv = require("dotenv").config();
+const cTable = require("console.table");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "costume88",
+  database: "company_DB"
+});
+
+//Put all these in a separate class file.
+function viewAllEmployees() {
+  connection.query("SELECT id, first_name, last_name FROM employee", function (err, res) {
+    if (err) throw err;
+    const allEmployees = res;
+    const table = cTable.getTable(allEmployees);
+    console.log(table);
+    menu();
+  });
+}
+
+function viewEmployees() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "How would you like to view personnel?",
+        choices: [
+          "View all employees",
+          "View by employee department",
+          "View by employee role",
+          "Return to main menu",
+        ],
+        name: "viewEmployees",
+      },
+    ])
+    .then((response) => {
+      const { viewEmployees } = response;
+
+      switch (viewEmployees) {
+        case "View all employees":
+          return viewAllEmployees();
+        case "View by employee department":
+          return //function
+        case "View by employee role":
+          return; //function;
+        case "Return to main menu":
+          menu();
+      }
+    });
+}
+
+function menu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message:
+          "Welcome to the Employee Tracker menu. What would you like to do?",
+        choices: [
+          "Add a department, role, or employee",
+          "View employees",
+          "Update employee role",
+          "Quit this menu",
+        ],
+        name: "menu",
+      },
+    ])
+    .then((response) => {
+      const { menu } = response;
+
+      switch (menu) {
+        case "Add a department, role, or employee":
+          return; //function;
+        case "View employees":
+          return viewEmployees();
+        case "Update employee roles":
+          return; //function;
+        case "Quit this menu":
+          connection.end();
+      }
+    });
+}
+
+connection.connect(function (err) {
+  if (err) throw err;
+  menu();
+});
